be80ca2e8b99dbe7c38b9384d7ea17f7f3a0b31d
Oleg Plugaru <119738558+OlegPlugaru@users.noreply.github.com>
2023-04-28 21:59:58 +0300

    Added a ball, a bar, and a funnel

--- GameCode.swift
+++ GameCode.swift
@@ -1,6 +1,27 @@
 import Foundation
 
 let circle = OvalShape(width: 150, height: 150)
+
+let barrierWidth = 300.0
+let barrierHeight = 25.0
+
+let barrierPoints = [
+    Point(x: 0, y: 0),
+    Point(x: 0, y: barrierHeight),
+    Point(x: barrierWidth, y: barrierHeight),
+    Point(x: barrierWidth, y: 0)
+]
+
+let barrier = PolygonShape(points: barrierPoints)
+
+let funnelPoints = [
+    Point(x: 0, y: 50),
+    Point(x: 80, y: 50),
+    Point(x: 60, y: 0),
+    Point(x: 20, y: 0)
+]
+
+let funnel = PolygonShape(points: funnelPoints)
 /*
 The setup() function is called once when the app launches. Without it, your app won't compile.
 Use it to set up and start your app.
@@ -16,4 +37,20 @@ func setup() {
     scene.add(circle)
     
     circle.hasPhysics = true
+    
+    // Add a barrier to the scene.
+    barrier.position = Point(x: 200, y: 150)
+    barrier.hasPhysics = true
+    scene.add(barrier)
+    barrier.isImmobile = true
+    
+    // Add a funnel to the scene.
+    funnel.position = Point(x: 200, y: scene.height - 25)
+    scene.add(funnel)
+    
+    funnel.onTapped = dropBall
+}
+
+func dropBall() {
+    circle.position = funnel.position
 }
