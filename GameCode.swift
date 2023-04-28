3617bc334931c877f600acff6c72c8b588fd1f6c
Oleg Plugaru <119738558+OlegPlugaru@users.noreply.github.com>
2023-04-28 21:39:42 +0300

    Initial Commit

---
+++ GameCode.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+let circle = OvalShape(width: 150, height: 150)
+/*
+The setup() function is called once when the app launches. Without it, your app won't compile.
+Use it to set up and start your app.
+
+You can create as many other functions as you want, and declare variables and constants,
+at the top level of the file (outside any function). You can't write any other kind of code,
+for example if statements and for loops, at the top level; they have to be written inside
+of a function.
+*/
+
+func setup() {
+    circle.position = Point(x: 250, y: 400)
+    scene.add(circle)
+    
+    circle.hasPhysics = true
+}
